394d43f58d3d43d82dbf647ecb3b86f7d2f65d60
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-07-25 17:07:36 -0400

    fix(onceTcpPortUsed): Handle ETIMEDOUT errors

--- src/ready-helpers/port-used.ts
+++ src/ready-helpers/port-used.ts
@@ -1,4 +1,7 @@
-import { waitUntilUsedOnHost } from 'tcp-port-used'
+import { promisify } from 'util'
+import { Socket } from 'net'
+
+const delay = promisify(setTimeout)
 
 /**
  * Waits until the given `port` (on the given `host`) is accepting connections
@@ -31,5 +34,36 @@ export async function onceTcpPortUsed(
   host = 'localhost',
 ): Promise<void> {
   const portNumber = typeof port === 'number' ? port : parseInt(port, 10)
-  await waitUntilUsedOnHost(portNumber, host, 250, 2147483647)
+  while (true) {
+    if (await isTcpPortUsed(portNumber, host)) {
+      return
+    } else {
+      await delay(250)
+    }
+  }
+}
+
+function isTcpPortUsed(port: number, host: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const socket = new Socket()
+    socket.once('connect', () => {
+      resolve(true)
+      cleanUp()
+    })
+    socket.once('error', error => {
+      if (['ECONNREFUSED', 'ETIMEDOUT'].includes((error as any).code)) {
+        resolve(false)
+      } else {
+        reject(error)
+      }
+      cleanUp()
+    })
+    function cleanUp() {
+      socket.removeAllListeners()
+      socket.end()
+      socket.destroy()
+      socket.unref()
+    }
+    socket.connect(port, host)
+  })
 }
