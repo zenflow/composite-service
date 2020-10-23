49999118fad5deb29c8c10f71d7950ecaa55205f
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-10-23 13:50:02 -0400

    chore(examples): Put example composite service scripts in examples dir

--- examples/shutdown.js
+++ examples/shutdown.js
@@ -1,12 +1,16 @@
-const { startCompositeService } = require('../..')
+const { startCompositeService } = require('..')
+
+const hang = process.argv.includes('--hang')
 
 const command = [
   'node',
   '-e',
   `\
+console.log('hi');
 setInterval(() => {});
 process.on('SIGINT', () => {
   console.log('got SIGINT');
+  if (${hang}) return;
   setTimeout(() => {
     console.log('bye');
     process.exit(130);
@@ -17,8 +21,12 @@ process.on('SIGINT', () => {
 startCompositeService({
   gracefulShutdown: true,
   windowsCtrlCShutdown: true,
+  serviceDefaults: {
+    command,
+    forceKillTimeout: 2000,
+  },
   services: {
-    foo: { command },
-    bar: { dependencies: ['foo'], command },
+    foo: {},
+    bar: { dependencies: ['foo'] },
   },
 })
